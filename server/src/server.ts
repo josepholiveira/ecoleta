9722bf04488bfd1b416f2bfa0341c715066ceff8
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2020-06-02 10:09:22 -0300

    :tada: Initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import routes from "./routes";
+import path from "path";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.listen(3333)
